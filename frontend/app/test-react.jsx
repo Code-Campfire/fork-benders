c560f84b2e2cce9471b06f45a42d48da6c185f72
Will MacLean <maclean.will@gmail.com>
2025-10-06 08:03:23 -0500

    Test JSX linting

---
+++ frontend/app/test-react.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestComponent = ({ name }) => {
+    return (
+        <div>
+            <h1>Hello, {name}!</h1>
+            <p>This tests JSX linting</p>
+        </div>
+    );
+};
+
+export default TestComponent;
